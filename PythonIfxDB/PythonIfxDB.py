7405a65a2823653048dcabe79448e8592012dc48
Satyan <ifmxdb@gmail.com>
2017-05-03 19:12:39 -0500

    cleanup

--- PythonIfxDB/PythonIfxDB.py
+++ PythonIfxDB/PythonIfxDB.py
@@ -6,7 +6,6 @@ import ifx_db
 #
 # netstat -a | findstr  9088
 ConStr = "SERVER=ids0;DATABASE=db1;HOST=127.0.0.1;PROTOCOL=onsoctcp;SERVICE=9088;UID=TestUser1;PWD=MySimplePass1;"
-ConStr = "SERVER=ids5;DATABASE=db1;HOST=lxvm-l170.lenexa.ibm.com;PROTOCOL=onsoctcp;SERVICE=5555;UID=informix;PWD=Ifmx4hcl"
 
 conn = ifx_db.connect( ConStr, "", "")
 

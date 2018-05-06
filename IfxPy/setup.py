5aea7a24381b18b0cb500401721a89572d94512d
Satyan <msatyan@gmail.com>
2018-05-05 21:56:00 -0500

    PyPi upload

--- IfxPy/setup.py
+++ IfxPy/setup.py
@@ -89,7 +89,7 @@ setup (name    = PACKAGE,
            'Topic :: Software Development',
 
            # Pick your license as you wish (should match "license" above)
-           'License :: Apache Version 2.0 License',
+           'License :: OSI Approved :: Apache Software License',
 
            # Specify the Python versions you support here. In particular, ensure
            # that you indicate whether you support Python 2, Python 3 or both.

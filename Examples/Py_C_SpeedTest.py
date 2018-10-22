474c6180d240c9021cc2c39d2512044c0147f4c3
Satyan <msatyan@hcl.com>
2018-10-22 12:19:41 -0500

    Sample

--- Examples/Py_C_SpeedTest.py
+++ Examples/Py_C_SpeedTest.py
@@ -1,7 +1,7 @@
 
-# Sample1.py
+# Py_C_SpeedTest.py
 import IfxPy
-# import IfxPyDbi as dbapi2
+import IfxPyDbi as dbapi2
 import datetime
 
 #################################################
@@ -42,53 +42,51 @@ def MySpeedTestWithPyPrimeCount(x, y):
     return PrimeCount
 
 
-
-def Main():
+###############################
+def Main( x, y ):
     print(' ')
-    print('Speed Test: C vs Pure Python code')
-    print('Please Wait....')
- 
-    x = 2
-    y = 30000
+    print("Speed Test: \'Pure Python\' vs \'Native Code\'. ")
+    print('Calculating Prime Numbers Between {:d} and {:d}'.format(x, y))
 
     print(' ')
-    print('C  functin, Calculating Prime numbers bwtween {:d} and {:d}'.format(x, y))
+    print('Executing C  function, please wait....' )
     s = datetime.datetime.now()
     rc3_c = IfxPy.SpeedTestWithCPrimeCount( x, y)
     e = datetime.datetime.now()
     delta_c = e - s
-    
-    print('# of Primes is {}'.format(rc3_c))
 
+    print('Number of Primes count is {}'.format(rc3_c))
 
-    #print(' ')
-    print('Py functin, Calculating Prime numbers bwtween {:d} and {:d}'.format(x, y))
+    print('Executing Py function, please wait....' )
     rc3 = 0
     s = datetime.datetime.now()
-    
-    # rc3 = dbapi2.SpeedTestWithPyPrimeCount( x, y)
-    rc3 = MySpeedTestWithPyPrimeCount( x, y)
+
+    rc3 = dbapi2.SpeedTestWithPyPrimeCount( x, y)
+
+    # Try enhancing Python function to improve the result, then you may use 'MySpeedTestWithPyPrimeCount()'
+    # rc3 = MySpeedTestWithPyPrimeCount( x, y)
+
     e = datetime.datetime.now()
     delta_py = e - s
-    # print('{}'.format(rc3))
 
     if rc3 == rc3_c:
-        # print('    To Count number of primes between {:d} and {:d} = {:d} '.format(x, y, rc3))
         print(' ')
         print('Time taken by :')
-        print('C  function = {};'.format(delta_c))
-        print('Py function = {};'.format(delta_py))
+        print('     Natice function = {};'.format(delta_c))
+        print('     Python function = {};'.format(delta_py))
         print(' ')
         r = delta_py / delta_c
-        print( "The C function appeared to be {0:.2f} times faster".format( r ))
+        print( "The Natice C function appeared to be {0:.2f} times faster".format( r ))
     else:
         print( "Error: Prime Count not matching C={} Py={}".format(rc3_c, rc3))
 
     print(' ')
 
 
+#########################################
 if __name__ == "__main__":
-    Main()
+    #Find the number of prime numbers between X and Y
+    Main( 2, 50000 )
 
 
 

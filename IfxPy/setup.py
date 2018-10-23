18c9c3d52f120f4f94698cc10b3b25796f430b7f
Satyan <msatyan@gmail.com>
2018-10-22 21:46:36 -0500

    New version for py 3.7 on win for release

--- IfxPy/setup.py
+++ IfxPy/setup.py
@@ -9,7 +9,7 @@ from distutils.core import Extension
 
 
 PACKAGE   = 'IfxPy'
-VERSION   = '3.0.2'
+VERSION   = '3.0.3'
 VERSION2X = '2.7.1'
 LICENSE   = 'Apache License 2.0'
 IfxPyLongDescription='Informix native Python driver is a high performing data access interface suitable for highly scalable enterprise and IoT solutions to works with Informix database.'
